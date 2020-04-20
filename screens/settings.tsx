e2ca873befdabd01c5d80c6a437f1fb4675a5111
pstrum <pstrum@gmail.com>
2020-04-20 00:55:39 -0700

    Add dummy settings screen

---
+++ screens/settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar, Text, View } from 'react-native';
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <StatusBar barStyle={'light-content'} />
+      <Text>Settings</Text>
+    </View>
+  );
+};
+
+export default SettingsScreen;
